cab75b476f3e5c6627df532413b4cbd0ce772cbd
Mateus Hiro Nagata <hiro@MateusnoMacBook-Pro-1611.local>
2026-02-27 10:31:05 -0300

    added Dowhy and iv

--- wooldridge.py
+++ wooldridge.py
@@ -35,15 +35,25 @@ for name, df in datasets.items():
 #0. Setup
 
 
-X = mroz[['exper',
+df = mroz[mroz['inlf'] == 1].copy().reset_index(drop=True)
+X = df[['exper',
           'expersq',
           'educ',
           'age',
           'kidslt6',
           'kidsge6']].copy().reset_index(drop=True)
 
-y = mroz['lwage']
+y = df['lwage']
 
+Y_col  = 'lwage'          # outcome: log(wage)
+T_col  = 'educ'           # treatment: years of education
+Z_cols = ['fatheduc',
+          'motheduc']     # instruments: parents' education
+X_cols = ['exper',
+          'expersq',
+          'age',
+          'kidslt6',
+          'kidsge6']      # controls
 
 
 #1. Drop NAs and do the simplest analysis
@@ -67,20 +77,72 @@ print(model.summary())
 
 #1.2. Heteroskedasticity robust!
 
-model_hc3  = sm.OLS(y,x).fit(cov_type='HC3')
+model_hc3  = sm.OLS(y,X).fit(cov_type='HC3')
 print(model_hc3.summary())
 
-#2. Instrumental Variables
-print("FIRST STAGE — instrument relevance:")
+#2. Checking Instrument relevance!!!
+
+# Check if instruments are relevant
 fs_X = sm.add_constant(df[Z_cols + X_cols])
 fs   = sm.OLS(df[T_col], fs_X).fit()
-fs_fstat = fs.fvalue
+print(fs.summary())
+fs_fstat = fs.fvalue #This is F-statistic
 print(f"  F-statistic on instruments: {fs_fstat:.2f}  "
       f"({'STRONG (>10)' if fs_fstat > 10 else 'WEAK'})")
-print(f"  fatheduc coef: {fs.params['fatheduc']:.4f}  p={fs.pvalues['fatheduc']:.4f}")
-print(f"  motheduc coef: {fs.params['motheduc']:.4f}  p={fs.pvalues['motheduc']:.4f}\n")
+
+#For custom joint hypotheses
+hypotheses = "age = 0, kidslt6 = 0"
+f_test = fs.f_test(hypotheses)
+print(f_test)
 
 
+
+#2.1 2SLS with linearmodels
+
+from linearmodels.iv import IV2SLS
+
+# Formula-based API
+iv_formula = (
+    f"{Y_col} ~ 1 + {' + '.join(X_cols)}"
+    f" [{T_col} ~ {' + '.join(Z_cols)}]"
+)
+iv_model = IV2SLS.from_formula(iv_formula, data=df).fit(cov_type='robust')
+
+print(iv_model.summary.tables[1]) #2SLS estimate of beta of educ 0.0578 (SE = 0.0337)
+# IV gives a lower estimate, consistent with bias pulling OLS upward
+
+hausman = iv_model.wooldridge_regression #endogeneity confirmed! educ is endogenous
+
+
+# 3. Dowhy
+import dowhy
+from dowhy import CausalModel
+
+causal_graph = """
+digraph {
+    educ        -> lwage;
+    exper       -> lwage;
+    expersq     -> lwage;
+    age         -> lwage;
+    kidslt6     -> lwage;
+    kidsge6     -> lwage;
+    fatheduc    -> educ;
+    motheduc    -> educ;
+    exper       -> educ;
+    age         -> educ;
+    U [label="Ability (unobserved)"];
+    U -> educ;
+    U -> lwage;
+}
+"""
+
+model_dowhy = CausalModel(
+    data=df,
+    treatment=T_col,
+    outcome=Y_col,
+    graph=causal_graph,
+    instruments=Z_cols
+)
 
 
 
